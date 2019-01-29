29ab639161b3380766e964e03cdd0f1fab97d419
OneOfManyMichaels <mikaerusan@gmail.com>
2019-01-29 15:54:59 -0800

    process.env fix for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,18 @@
 //  * Inside the `connection.js` file, setup the code to connect Node to MySQL.
 var mysql = require("mysql");
+var connection;
 
-
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = msqy.createConnection(process.env.JAWSDB_URL);
+} else {
+ connection = mysql.createConnection({
     port: 3306,
     host: "localhost",
     user: "root",
     password: "root",
     database: "burgers_db"
   });
-
+};
 // Make connection.
 connection.connect(function(err) {
   if (err) {
